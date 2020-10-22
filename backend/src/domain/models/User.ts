3898dc260ec89c997e34acbe4251f13fa70d818c
Luis Felipe Fagundes <luisfelipecf94@gmail.com>
2020-10-22 12:31:43 -0300

    implementando a entidade Usuário

--- backend/src/domain/models/User.ts
+++ backend/src/domain/models/User.ts
@@ -1,3 +1,45 @@
-class User {
+import {
+  Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn,
+} from 'typeorm'
+import AccountType from '../enums/AccountType'
 
+@Entity('users')
+class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @Column('text')
+  accountType: AccountType
+
+  @Column()
+  name: string
+
+  @Column()
+  email: string
+
+  @Column()
+  password: string
+
+  @Column()
+  phone: string
+
+  @Column()
+  state: string
+
+  @Column()
+  city: string
+
+  @Column()
+  cpf: string
+
+  @Column()
+  cnpj: string
+
+  @CreateDateColumn()
+  createdAt: Date
+
+  @UpdateDateColumn()
+  updatedAt: Date
 }
+
+export default User
