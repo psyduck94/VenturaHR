c54c6434f29caf03e584ed3fc75c1b56da1c7888
Luis Felipe Fagundes <luisfelipecf94@gmail.com>
2020-10-22 12:05:05 -0300

    implementando a migration CreateUsers para o banco de dados

---
+++ backend/src/database/migrations/1603376940441-CreateUsers.ts
@@ -0,0 +1,81 @@
+import { formatDistanceToNow } from 'date-fns'
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export default class CreateUsers1603376940441 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'accountType',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isNullable: false,
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'phone',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'state',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'city',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'cpf',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'cnpj',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updatedAt',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users')
+  }
+}
