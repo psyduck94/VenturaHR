a43210a76502d4b4af7627df2f09094d75ee2e43
Luis Felipe Fagundes <luisfelipecf94@gmail.com>
2020-11-07 13:44:50 -0300

    validando os campos de login

--- web/venturahr/src/pages/Login/index.tsx
+++ web/venturahr/src/pages/Login/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useRef, useCallback } from 'react'
+import * as Yup from 'yup'
 import logo from '../../assets/app_logo.jpg'
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
 import {
@@ -8,25 +9,42 @@ import {
 } from './styles'
 import Button from '../../components/Button'
 import Input from '../../components/Input'
+import { FormHandles } from '@unform/core'
 import { Form } from '@unform/web'
+import getValidationErrors from '../../utils/getValidationErrors'
 
 /* Classe responsável pela UI da página de Login */
 
 const Login: React.FC = () => {
+    const formRef = useRef<FormHandles>(null)
 
-    function handleSubmit(data: object): void {
-        console.log(data)
-    }
+    const handleSubmit = useCallback(async (data: object) => {
+        try {
+            formRef.current?.setErrors({})
+
+            const schema = Yup.object().shape({
+                email: Yup.string().required('E-mail obrigatório').email('Digite um e-mail válido'),
+                password: Yup.string().required('Senha obrigatório'),
+            })
+
+            await schema.validate(data, {
+                abortEarly: false,
+            })
+        } catch (err) {
+            const errors = getValidationErrors(err)
+            formRef.current?.setErrors(errors)
+        }
+    }, [])
 
     return (
         <>
             <Container>
                 <Content>
                     <img src={logo} alt="Logo da Empresa" width="150" />
-                    <Form onSubmit={handleSubmit}>
+                    <Form ref={formRef} onSubmit={handleSubmit}>
                         <h2>Faça seu login</h2>
                         <Input name="email" icon={FiMail} placeholder="E-mail" />
-                        <Input name="password"  icon={FiLock} placeholder="Senha" type="password" />
+                        <Input name="password" icon={FiLock} placeholder="Senha" type="password" />
                         <Button type="submit">Entrar</Button>
                         <a href="forgot">Esqueci minha senha</a>
                     </Form>
